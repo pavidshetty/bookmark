c43e02f5f5e57c37d74a0cee468183e1885e78fd
pavidshetty <pavitrashetty23@gmail.com>
2019-11-28 15:42:47 +0000

    refactoring the test

--- spec/features/view_bookmarks_spec.rb
+++ spec/features/view_bookmarks_spec.rb
@@ -1,11 +1,15 @@
 require 'pg'
 feature 'Bookmarks' do
   scenario 'returns the list of bookmarks' do
-    connection = PG.connect(dbname: 'bookmark_manager_test')
+    # connection = PG.connect(dbname: 'bookmark_manager_test')
     # Add the test data
-    connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
-    connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.destroyallsoftware.com');")
-    connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com');")
+    # connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
+    # connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.destroyallsoftware.com');")
+    # connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com');")
+
+    Bookmark.create(url: "http://www.makersacademy.com")
+    Bookmark.create(url: "http://www.destroyallsoftware.com")
+    Bookmark.create(url: "http://www.google.com")
     visit('/bookmarks')
 
     expect(page).to have_content 'http://www.google.com'
